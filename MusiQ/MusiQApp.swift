59d45686549dd5823d519518db3630ad1abd4251
Sriram P H <dukeonline98@outlook.com>
2025-01-25 21:44:07 -0500

    Initial Commit

---
+++ MusiQ/MusiQApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiQApp.swift
+//  MusiQ
+//
+//  Created by Sriram P H on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MusiQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusiQTests/MusiQTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusiQTests.swift
+//  MusiQTests
+//
+//  Created by Sriram P H on 1/25/25.
+//
+
+import Testing
+@testable import MusiQ
+
+struct MusiQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
